495f4829672847d7c19369811a8992001bed1939
Zakaria Ech-chifaouy <machinelearning1.618@gmail.com>
2023-11-18 12:26:43 +0100

    1337

--- get_next_line.h
+++ get_next_line.h
@@ -6,22 +6,23 @@
 /*   By: zech-chi <zech-chi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 11:20:14 by zech-chi          #+#    #+#             */
-/*   Updated: 2023/11/18 12:11:11 by zech-chi         ###   ########.fr       */
+/*   Updated: 2023/11/18 12:26:24 by zech-chi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
+# ifndef BUFFER_SIZE
 # define BUFFER_SIZE 1
-#endif
+# endif
 
-#include <unistd.h>
-#include <stdlib.h>
+# include <unistd.h>
+# include <stdlib.h>
 
 size_t	ft_strlen(char *str);
 char	*ft_strdup(char *str);
 char	*ft_strjoin(char *s1, char *s2);
+char	*get_next_line(int fd);
 
 #endif
