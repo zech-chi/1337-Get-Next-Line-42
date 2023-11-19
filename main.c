1642c5d5fa3f9763e650ed0b1dab9ae39fb3482f
Zakaria Ech-chifaouy <machinelearning1.618@gmail.com>
2023-11-19 14:35:41 +0100

    1337

--- main.c
+++ main.c
@@ -5,12 +5,16 @@
 
 
 int main() {
-	int fd = open("/Users/zech-chi/Desktop/get_next_line/testfiles/file.txt", O_RDONLY);
-	char	*line;
-	for (int i = 0; i < 1; i++)
-	{
-		line = get_next_line(fd);
-		printf("%s\n", line);
-	}
+	int fd = open("/Users/zech-chi/Desktop/get_next_line/testfiles/maze.txt", O_RDONLY);
+	//char	*line;
+	//for (int i = 0; i < 2; i++)
+	//{
+	//	line = get_next_line(fd);
+	//	printf("%s", line);
+	//	if (line)
+	//		free(line);
+	//}
+	int j = read(fd, buff, 0);
+	printf("%d", j);
 	return (0);
 }
